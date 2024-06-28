cf4be76db4402d4e042eaba9066116469abf953d
JORGE <marialuci1019@gmail.com>
2024-06-27 21:26:40 -0500

    Se completa el modelo de roles y permisos con usuario y se agregan los authorities con base en los nuevos roles asociados

--- src/main/java/co/com/security/seguridad_jwt/entity/PermisosEntity.java
+++ src/main/java/co/com/security/seguridad_jwt/entity/PermisosEntity.java
@@ -2,6 +2,7 @@ package co.com.security.seguridad_jwt.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.HashSet;
@@ -10,19 +11,15 @@ import java.util.Set;
 @Entity
 @Getter
 @Setter
-public class UserEntity {
+@NoArgsConstructor
+public class PermisosEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String userName;
-    private String password;
 
-    @ManyToMany(fetch = FetchType.EAGER)
-    @JoinTable(
-            name = "roles_usuario",
-            joinColumns = @JoinColumn(name = "user_id"),
-            inverseJoinColumns = @JoinColumn(name = "rol_id")
-    )
+    private String nombre;
+
+    @ManyToMany(mappedBy = "permisos")
     private Set<RolEntity> roles = new HashSet<>();
 }
--- src/main/java/co/com/security/seguridad_jwt/services/CustomUserDetailsService.java
+++ src/main/java/co/com/security/seguridad_jwt/services/CustomUserDetailsService.java
@@ -3,12 +3,17 @@ package co.com.security.seguridad_jwt.services;
 import co.com.security.seguridad_jwt.entity.UserEntity;
 import co.com.security.seguridad_jwt.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
@@ -19,11 +24,13 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         UserEntity user = userRepository.findByUserName(username);
-        if(user == null){
+        if (user == null) {
             throw new UsernameNotFoundException("Usuario no encontrado");
         }
-        return new User(user.getUserName(),
-                        user.getPassword(),
-                        AuthorityUtils.createAuthorityList(user.getRole()));
+        Set<GrantedAuthority> authorities = user.getRoles().stream()
+                .map(rol -> new SimpleGrantedAuthority(rol.getNombre()))
+                .collect(Collectors.toSet());
+
+        return new User(user.getUserName(), user.getPassword(), authorities);
     }
 }
